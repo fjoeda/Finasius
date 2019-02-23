b218d97712a9963ed1e646a883cd7243e7fb28ca
fjoeda <f.yudanto@gmail.com>
2019-02-23 18:38:55 +0700

    navigation

--- app/src/main/java/com/nl/manage/finasius/MainMenuActivity.java
+++ app/src/main/java/com/nl/manage/finasius/MainMenuActivity.java
@@ -1,13 +1,78 @@
 package com.nl.manage.finasius;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import devlight.io.library.ntb.NavigationTabBar;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main_menu);
+        NavigationTabBar navigationTabBar = (NavigationTabBar)findViewById(R.id.ntb);
+
+        ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_launcher_background),
+                        Color.argb(255,12,144,32)
+                ).title("Heart")
+                        .badgeTitle("NTB")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_launcher_background),
+                        Color.argb(255,12,144,32)
+                ).title("Cup")
+                        .badgeTitle("with")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_launcher_background),
+                        Color.argb(255,12,44,222)
+                ).title("Diploma")
+                        .badgeTitle("state")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_launcher_background),
+                        Color.argb(255,12,44,32)
+                ).title("Flag")
+                        .badgeTitle("icon")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_launcher_background),
+                        Color.argb(255,122,144,32)
+                ).title("Medal")
+                        .badgeTitle("777")
+                        .build()
+        );
+        navigationTabBar.setModels(models);
+        navigationTabBar.setTitleMode(NavigationTabBar.TitleMode.ACTIVE);
+        navigationTabBar.setBadgeGravity(NavigationTabBar.BadgeGravity.BOTTOM);
+        navigationTabBar.setBadgePosition(NavigationTabBar.BadgePosition.CENTER);
+        navigationTabBar.setTypeface("fonts/custom_font.ttf");
+        navigationTabBar.setIsBadged(true);
+        navigationTabBar.setIsTitled(true);
+        navigationTabBar.setIsTinted(true);
+        navigationTabBar.setIsBadgeUseTypeface(true);
+        navigationTabBar.setBadgeBgColor(Color.RED);
+        navigationTabBar.setBadgeTitleColor(Color.WHITE);
+        navigationTabBar.setIsSwiped(true);
+        navigationTabBar.setBgColor(Color.BLACK);
+        navigationTabBar.setBadgeSize(10);
+        navigationTabBar.setTitleSize(10);
+        navigationTabBar.setIconSizeFraction(0.5f);
     }
 }
