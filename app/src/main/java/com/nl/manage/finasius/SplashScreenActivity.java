83cf8b1278c377fc86367c94eab35f017f8e8a58
fjoeda <f.yudanto@gmail.com>
2019-02-23 17:50:02 +0700

    start

---
+++ app/src/main/java/com/nl/manage/finasius/SplashScreenActivity.java
@@ -0,0 +1,57 @@
+package com.nl.manage.finasius;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(2*1000);
+
+                    Intent intent = new Intent(SplashScreenActivity.this,MainMenuActivity.class);
+                    startActivity(intent);
+
+
+                    // After 5 seconds redirect to another intent
+
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
